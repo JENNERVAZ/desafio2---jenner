6665b0a707f38e00b1b70db547fe46ad28690888
Jenner Vaz <90334151+JENNERVAZ@users.noreply.github.com>
2021-09-08 11:47:22 -0300

    Arquivos iniciais para o projeto MakeCode

---
+++ main.ts
@@ -0,0 +1,24 @@
+let Graus = 0
+input.onButtonPressed(Button.A, function () {
+    Graus = input.compassHeading()
+})
+basic.forever(function () {
+    Graus = input.compassHeading()
+    if (Graus < 45) {
+        basic.showString("N")
+    } else {
+        if (Graus < 135) {
+            basic.showString("L")
+        } else {
+            if (Graus < 225) {
+                basic.showString("S")
+            } else {
+                if (Graus < 315) {
+                    basic.showString("O")
+                } else {
+                    basic.showString("N")
+                }
+            }
+        }
+    }
+})
